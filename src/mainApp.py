7aad30ebb6abce3d6dea593ef6fad62efc52b99b
Iwan Dwi Prakoso <55144122+iwandepe@users.noreply.github.com>
2020-10-17 22:19:46 +0700

    Update mainApp.py

--- src/mainApp.py
+++ src/mainApp.py
@@ -1,37 +1,31 @@
-from obe import *
-from os import system, name
-from time import sleep
+from autoOBE import *
+from manualOBE import *
 
-print("PROGRAM OPERASI MATRIX DENGAN OBE\n")
+np.set_printoptions(suppress=True)
+
+print("PROGRAM MENCARI INVERS MATRIX DENGAN OBE\n")
 
 print("Matrix 3x3\n")
-# mengganti isi matrix dengan hardcode
-matrix=np.mat([[3,1,2],[0,1,-5.],[1,1,-1]])
-while True:
-    print(matrix, end="\n\n")
-    opsi = input().strip()
-    
-    if opsi == "out":
-        break
-    elif opsi == "tb":
-        print("Masukkan 2 baris yang akan ditukar")
-        print("input: ", end='')
-        baris1, baris2 = map(int, input().split())
-        tukarBaris(matrix, baris1, baris2)
-    elif opsi == "tk":
-        print("Masukkan 2 kolom yang akan ditukar")
-        print("masukan: ", end='')
-        kolom1, kolom2 = map(int, input().split())
-        tukarKolom(matrix, kolom1, kolom2)
-    elif opsi == "ob":
-        print("Masukkan baris tujuan, baris awal, dan N kali")
-        print("input: ", end='')
-        barisTujuan, barisAwal, nKali = input().split()
-        operasiBaris(matrix, int(barisTujuan), int(barisAwal), eval(nKali))
-    elif opsi == "kali":
-        print("Masukkan baris dan N kali")
-        print("input: ", end='')
-        baris, nKali = input().split()
-        kaliN(matrix, int(baris), eval(nKali))
-    else:
-        print("Masukan tidak valid, ulangi")
+
+# Deklarasi isi matrix dengan hardcode
+matrix = np.mat([[29.,25.,-16.],[25.,75.,-15.],[-16.,-15.,73.]])
+I = np.eye(3)
+size = 3
+
+# Gabungkan matrix dengan matrix identitas
+matrix = np.hstack([matrix,I])  
+
+# Cetak soal
+print(matrix, end="\n\n")
+
+print("PILIHAN:")
+print("1. Manual")
+print("2. Auto", end='\n\n')
+print("Masukkan pilihan: ", end='')
+
+option = int(input().strip())
+
+if option == 1:
+    manualInvers(matrix)
+else:
+    autoInvers(matrix, size)
